ae1cbeac2235fb7e87964b382c38ea0340beca0a
Aditya Mitra <adityaarghya0@gmail.com>
2026-03-23 14:27:51 +0300

    Update monitor.py

--- SSHMonitor/monitor.py
+++ SSHMonitor/monitor.py
@@ -9,6 +9,9 @@ authfile='/var/log/auth.log'
 blocking=[]
 blocked={}
 
+MAX_BUF=200
+buf=[]
+
 def get_users():
 	users=[]
 	with open('/etc/passwd', 'r') as file:
@@ -30,22 +33,37 @@ def block_user(username):
 	blocked[username]=int(datetime.datetime.now().timestamp())
 	blocking.remove(username)
 
+def check_log_line(linex):
+	try:
+		line=linex.decode(errors='ignore')
+		processed=convert_lines([line])
+		if len(processed)<1:
+			continue
+		log=processed[0]
+		result=detector.ingest_log(log)
+		if log['prediction']['alert']:
+			print(f'Probable brute force on {log["username"]}')
+			threading.Thread(target=block_user, args=(log['username'],)).start()
+	except:
+		print('Error processing log')
+
+def check_buf():
+	while True:
+		try:
+			if len(buf)> MAX_BUF:
+				buf=[]
+			while len(buf)>1:
+				linex=buf.pop(0)
+				check_log_line(linex)
+		except:
+			pass
+
 def monitor():
 	proc=subprocess.Popen(f'tail -n 0 -f {authfile}'.split(), stdout=subprocess.PIPE)
 	print('Monitoring')
 	for linex in proc.stdout:
-		try:
-			line=linex.decode(errors='ignore')
-			processed=convert_lines([line])
-			if len(processed)<1:
-				continue
-			log=processed[0]
-			result=detector.ingest_log(log)
-			if log['prediction']['alert']:
-				print(f'Probable brute force on {log["username"]}')
-				threading.Thread(target=block_user, args=(log['username'],)).start()
-		except:
-			print('Error processing log')
-
+		buf.append(linex)
+		
 if __name__=='__main__':
+	threading.Thread(target=check_buf).start()
 	monitor()
